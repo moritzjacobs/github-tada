81b72034bd4e7cc22ccb52ece1636beb4c55e758
Moritz Jacobs <mail@moritzjacobs.de>
2019-06-18 09:02:06 +0200

    WIP

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ const CLASS_WRAPPER = "grb-browser";
 const CLASS_BUTTON = "grb-browser-button";
 const CLASS_HIGHLIGHT = "grb-highlight";
 const CLASS_REACTION = "grb-reaction";
+const REACTION_THRESHOLD = 0.25;
+const COMMENT_THRESHOLD = 0.025;
 
 // run!
 githubTada();
@@ -16,6 +18,7 @@ githubTada();
 /**
  * Main function
  */
+
 function githubTada() {
 	// DOM cache
 
@@ -29,24 +32,39 @@ function githubTada() {
 
 	// assemble all relevant data and remove empty results
 	const comments = [...commentsEls].map(commentParser).filter(comment => {
-		return comment.reactions.length > 0;
+		return comment.reactions.total > 0;
 	});
+
+	const superTotal = comments.reduce((sum, c) => {
+		sum += c.reactions.total;
+
+		return sum;
+	}, 0);
 
 	// create button for every comment with reactions
 	comments.forEach(comment => {
 		const button = document.createElement("button");
 
 		button.classList.add(CLASS_BUTTON);
+		const commentImportance = comment.reactions.total / superTotal;
+
+		if (commentImportance < COMMENT_THRESHOLD) {
+			return;
+		}
 
 		// add every reaction to the button
-		comment.reactions.forEach(r => {
+		comment.reactions.single.forEach(r => {
 			if (!r) {
 				return;
 			}
 			const reactionEl = document.createElement("span");
+			const em = commentImportance * r.fraction * 10;
 
+			console.log(em);
 			reactionEl.classList.add(CLASS_REACTION);
-			reactionEl.innerText += `${r.emoji} ${r.count}`;
+			reactionEl.innerText += `${r.emoji}`;
+			reactionEl.dataset.grbCount = r.count;
+			reactionEl.style.fontSize = `${em}em`;
 			button.appendChild(reactionEl);
 		});
 
@@ -67,7 +85,23 @@ function githubTada() {
  */
 function commentParser(el) {
 	const reactionEls = el.querySelectorAll(SELECTOR_REACTION);
-	const reactions = [...reactionEls].map(reactionParser);
+	const reactions = {single: [...reactionEls].map(reactionParser)};
+
+	// calculate the total of reactions
+	reactions.total = reactions.single.reduce((sum, r) => {
+		return sum + r.count;
+	}, 0);
+
+	// calculate fraction of each reaction of total of reactions
+	reactions.single = reactions.single.map(r => {
+		const fraction = (r.count / reactions.total);
+
+		return {...r, fraction};
+	});
+
+	reactions.single = reactions.single.filter(r => r.fraction > REACTION_THRESHOLD);
+
+	reactions.single.sort((a, b) => b.count - a.count);
 
 	return {
 		el: el.querySelector(SELECTOR_COMMENT),
