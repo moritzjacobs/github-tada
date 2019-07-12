60ef9acf8d31abb7d7390b68eadfec0deadacc18
Moritz Jacobs <mail@moritzjacobs.de>
2019-07-12 22:40:23 +0200

    Improve css handling in webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,10 +1,30 @@
+/* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable import/unambiguous */
+
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
 module.exports = {
+	plugins: [
+		new MiniCssExtractPlugin({
+			filename: "[name].css",
+			chunkFilename: "[id].css",
+		}),
+	],
 	module: {
 		rules: [
 			{
 				test: /\.scss$/,
-				use: ["style-loader", "css-loader", "sass-loader"],
+				use: [
+					{
+						loader: MiniCssExtractPlugin.loader,
+						options: {
+							publicPath: "../",
+							hmr: process.env.NODE_ENV === "development",
+						},
+					},
+					"css-loader",
+					"sass-loader",
+				],
 			},
 		],
 	},
