b0dfb2b05ed732a45100d0f28fbbd9091b4cf248
Moritz Jacobs <mail@moritzjacobs.de>
2019-07-12 15:31:57 +0200

    Redesign :D

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import {
 	CLASS_BUTTON,
 	CLASS_REACTION,
 	CLASS_WRAPPER,
-	COMMENT_THRESHOLD,
 	SELECTOR_COMMENT_CONTAINER,
 	SELECTOR_TARGET,
 } from "./constants";
@@ -19,42 +18,25 @@ buttons.classList.add(CLASS_WRAPPER);
 // grab all relevant DOM elements
 const commentsEls = document.querySelectorAll(SELECTOR_COMMENT_CONTAINER);
 const header = document.querySelector(SELECTOR_TARGET);
+// assemble all relevant data
+const comments = [...commentsEls].map(commentParser);
 
-// assemble all relevant data and remove empty results
-const comments = [...commentsEls].map(commentParser).filter(comment => {
-	return comment.reactions.total > 0;
-});
+console.log(comments);
 
-const superTotal = comments.reduce((sum, c) => {
-	sum += c.reactions.total;
-
-	return sum;
-}, 0);
-
-// create button for every comment with reactions
+// create button for every comment
 comments.forEach(comment => {
 	const button = document.createElement("button");
 
 	button.classList.add(CLASS_BUTTON);
-	const commentImportance = comment.reactions.total / superTotal;
-
-	if (commentImportance < COMMENT_THRESHOLD) {
-		return;
-	}
 
 	// add every reaction to the button
 	comment.reactions.single.forEach(r => {
-		if (!r) {
-			return;
-		}
 		const reactionEl = document.createElement("span");
-		const em = commentImportance * r.fraction * 10;
 
-		console.log(em);
 		reactionEl.classList.add(CLASS_REACTION);
 		reactionEl.innerText += `${r.emoji}`;
 		reactionEl.dataset.grbCount = r.count;
-		reactionEl.style.fontSize = `${em}em`;
+
 		button.appendChild(reactionEl);
 	});
 
