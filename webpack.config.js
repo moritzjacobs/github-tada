049f41263c0f7342c8a08e9f29170f8f206769f8
Moritz Jacobs <mail@moritzjacobs.de>
2019-07-12 15:13:16 +0200

    Move css to scss

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,8 @@ module.exports = {
 	module: {
 		rules: [
 			{
-				test: /\.css$/,
-				use: ["style-loader", "css-loader"],
+				test: /\.scss$/,
+				use: ["style-loader", "css-loader", "sass-loader"],
 			},
 		],
 	},
