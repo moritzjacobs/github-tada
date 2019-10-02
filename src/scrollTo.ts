42445127b5e434df372ea9be305fdac5686d2bc3
Moritz Jacobs <mail@moritzjacobs.de>
2019-10-02 16:32:38 +0200

    cleanup

--- src/scrollTo.ts
+++ src/scrollTo.ts
@@ -1,18 +1,18 @@
-import { CLASS_HIGHLIGHT } from "./constants";
-import { Comment } from "./comments";
+import {CLASS_HIGHLIGHT} from "./constants";
 
 // Click handler: scroll into view
 const scrollTo = (targetCommentEl: HTMLElement): void => {
-  targetCommentEl.scrollIntoView({ behavior: "smooth", block: "center" });
+	targetCommentEl.scrollIntoView({behavior: "smooth", block: "center"});
 
-  // remove highlight from any other comment ...
-  const highlighted = document.querySelectorAll(CLASS_HIGHLIGHT);
-  for (const item of highlighted) {
-    item.classList.remove(CLASS_HIGHLIGHT);
-  }
+	// remove highlight from any other comment ...
+	const highlighted = document.querySelectorAll(CLASS_HIGHLIGHT);
 
-  // and add highlight to this comment
-  targetCommentEl.classList.add(CLASS_HIGHLIGHT);
+	for (const item of highlighted) {
+		item.classList.remove(CLASS_HIGHLIGHT);
+	}
+
+	// and add highlight to this comment
+	targetCommentEl.classList.add(CLASS_HIGHLIGHT);
 };
 
 export default scrollTo;
