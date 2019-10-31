dff01ab0e00e41d01f283a471527d3f6935d1c6a
Moritz Jacobs <mail@moritzjacobs.de>
2019-10-31 13:41:57 +0100

    Stricter types, better tests pt. 1

--- src/comments.ts
+++ src/comments.ts
@@ -17,18 +17,18 @@ export type Comment = {
 };
 
 // Extract relevant information from single reaction
-const reactionParser = (el: HTMLButtonElement): Reaction | null => {
+const reactionParser = (el: HTMLButtonElement): Reaction => {
 	const type = el.value;
 	const emoji = el.querySelector(SELECTOR_EMOJI);
+	const textContent = el && el.textContent ? el.textContent : "";
 
-	if (!emoji || !emoji.textContent) {
-		return null;
-	}
+	const emojiTextContent =
+		emoji && emoji.textContent ? emoji.textContent : "";
 
-	const count = parseInt(el.textContent.replace(emoji.textContent, ""));
+	const count = parseInt(textContent.replace(emojiTextContent, ""));
 
 	return {
-		emoji: emoji.textContent,
+		emoji: emojiTextContent,
 		type,
 		count,
 	};
@@ -36,27 +36,47 @@ const reactionParser = (el: HTMLButtonElement): Reaction | null => {
 
 // Extract relevant information from comment
 const commentParser = (el: HTMLElement): Comment => {
-	const reactionEls = Array.from(el.querySelectorAll(SELECTOR_REACTION));
+	const reactionNodeList = el.querySelectorAll<HTMLButtonElement>(
+		SELECTOR_REACTION
+	);
+
+	const reactionEls = Array.from(reactionNodeList);
 
 	const reactions: Array<Reaction> = reactionEls
 		.map(reactionParser)
-		.filter(r => Boolean(r));
+		.filter((r: Reaction) => Boolean(r));
 
 	reactions.sort((a, b) => b.count - a.count);
 
+	const target = el.querySelector<HTMLElement>(SELECTOR_COMMENT);
+
+	if (!target) {
+		throw new Error("no parseable target div found");
+	}
+
 	return {
-		target: el.querySelector(SELECTOR_COMMENT),
+		target,
 		reactions,
 	};
 };
 
 // parse comments from DOM and return data structure
 const getComments = (): Array<Comment> => {
-	const commentsEls = document.querySelectorAll(SELECTOR_COMMENT_CONTAINER);
+	try {
+		const commentsEls = document.querySelectorAll<HTMLElement>(
+			SELECTOR_COMMENT_CONTAINER
+		);
 
-	return Array.from(commentsEls, commentParser).filter(
-		comment => comment.reactions && comment.reactions.length > 0
-	);
+		const arr = Array.from(commentsEls, commentParser);
+
+		return arr.filter(
+			(comment: Comment) => comment.reactions && comment.reactions.length > 0
+		);
+	} catch (error) {
+		console.error(error);
+
+		return [];
+	}
 };
 
 export default getComments;
